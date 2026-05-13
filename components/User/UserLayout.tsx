32bdc6a6271eb6877593e05df694c1fbbbf9f9ee
johncarlodelrosario <c.delrosario@fyberblizz.com>
2026-05-13 11:40:02 +0800

    “update”

--- components/User/UserLayout.tsx
+++ components/User/UserLayout.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import Sidebar from "./Sidebar";
+import Sidebar from "./Sidebar"; // This imports the correct user sidebar
 import { FiMenu, FiBell } from "react-icons/fi";
 
 interface UserLayoutProps {
